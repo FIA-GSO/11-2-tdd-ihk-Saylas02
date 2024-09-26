e46f6592d0c10c97148c932a22a4c3307e2ffc02
Silas Hermes <silas.hermes@tuv.com>
2024-09-26 14:08:16 +0200

    setup positive and negative tests

--- test_berechnungsmodul.py
+++ test_berechnungsmodul.py
@@ -1,10 +1,28 @@
-import unittest
+import pytest
+import berechnungsmodul
+
+positive_testdata = [
+    (40, 50, 80),
+    (0, 50, 0),
+    (50, 50, 100),
+    (33, 50, 66),
+    (25, 50, 50)
+]
+
+negative_testdata = [
+    (" ", 1, TypeError),
+    (1, "", TypeError),
+    (1, 0, ValueError),
+    (-1, 0, ValueError),
+    (101, 100, ValueError)
+]
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+@pytest.mark.parametrize("erreichterWert", "maxWert, expected", positive_testdata)
+def test_berechne_prozente__positive(erreichterWert, maxWert, expected):
+    assert berechnungsmodul.berechne_prozente(erreichterWert, maxWert) == expected
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize("erreichterWert", "maxWert, expected", negative_testdata)
+def test_berechne_prozente__positive(erreichterWert, maxWert, expected):
+    assert berechnungsmodul.berechne_prozente(erreichterWert, maxWert) == expected
