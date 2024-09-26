7a37944721f4b6977aef7efc5b01f4f71fa97463
Silas Hermes <silas.hermes@tuv.com>
2024-09-26 14:31:28 +0200

    implementiere berechnungsmodul.berechne_prozente und adde todo für Matrix

---
+++ berechnungsmodul.py
@@ -0,0 +1,5 @@
+def berechne_prozente(erreichtePunkte: int, möglichePunkte:int) -> int:
+    return möglichePunkte // erreichtePunkte
+
+def berechne_note_von_Prozent(erreichte_Prozent: int) -> int:
+    """TODO: erstelle Matrix für Prozente"""
