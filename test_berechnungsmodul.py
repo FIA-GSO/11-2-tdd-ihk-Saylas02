109d934c1bfaea6fca29e862f9e5f789551a6ea7
Silas Hermes <silas.hermes@tuv.com>
2024-09-26 14:30:44 +0200

    erweitere tests für notenberechnung

--- test_berechnungsmodul.py
+++ test_berechnungsmodul.py
@@ -18,11 +18,41 @@ negative_testdata = [
 ]
 
 
-@pytest.mark.parametrize("erreichterWert", "maxWert, expected", positive_testdata)
+@pytest.mark.parametrize("erreichterWert, maxWert, expected", positive_testdata)
 def test_berechne_prozente__positive(erreichterWert, maxWert, expected):
     assert berechnungsmodul.berechne_prozente(erreichterWert, maxWert) == expected
 
 
-@pytest.mark.parametrize("erreichterWert", "maxWert, expected", negative_testdata)
-def test_berechne_prozente__positive(erreichterWert, maxWert, expected):
+@pytest.mark.parametrize("erreichterWert, maxWert, expected", negative_testdata)
+def test_berechne_prozente__negative(erreichterWert, maxWert, expected):
     assert berechnungsmodul.berechne_prozente(erreichterWert, maxWert) == expected
+
+
+positive_testdata = [
+    (100, "sehr gut"),
+    (92, "sehr gut"),
+    (91, "gut"),
+    (81, "gut"),
+    (80, "befriedigend"),
+    (67, "befriedigend"),
+    (66, "ausreichend"),
+    (50, "ausreichend"),
+    (49, "mangelhaft"),
+    (30, "mangelhaft"),
+    (29, "ungenügend"),
+    (0, "ungenügend")
+]
+
+negative_testdata = [
+    (" ", TypeError)
+]
+
+
+@pytest.mark.parametrize("erreichteProzent, expected", positive_testdata)
+def test_berechne_note_von_prozent__positive(erreichteProzent, expected):
+    assert berechnungsmodul.test_berechne_note_von_Prozent(erreichteProzent) == expected
+
+
+@pytest.mark.parametrize("erreichteProzent, expected", negative_testdata)
+def test_berechne_note_von_prozent__negative(erreichteProzent, expected):
+    assert berechnungsmodul.test_berechne_note_von_Prozent(erreichteProzent) == expected
